24f8d28b207a7d3bffe81d3b3ccd4a41d087e261
javed sayed <javedsayed@javeds-MacBook-Air.local>
2026-05-11 17:41:28 +0530

    Build: Ignored linting and TS checks for faster deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
